861d778b6ea439bf20ac1ed5b13103e654cda5cd
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-11-11 02:52:02 +0800

    feat(ui): add main menu and help screens

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
     import './app.css';
+
+    const { children } = $props();
 </script>
 
-<slot />
+<div class="flex h-screen items-center justify-center p-12">
+    {@render children()}
+</div>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<p>Hello world!</p>
+<script>
+    import Button from '$lib/Button.svelte';
+</script>
+
+<div class="flex flex-col items-center">
+    <h1 class="p-8 text-6xl">Zip Zap Zop!</h1>
+    <div class="grid grid-flow-col gap-4">
+        <a href="/lobbies"><Button content={'Play'} /></a>
+        <a href="/help"><Button content={'Help'} /></a>
+    </div>
+</div>
