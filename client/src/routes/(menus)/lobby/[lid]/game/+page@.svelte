ff945ca3db14de259d9a720e0cf693cf5517ffca
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-11-11 23:02:54 +0800

    feat(ui): add barebones game screen

--- client/src/routes/(menus)/lobby/[lid]/game/+page@.svelte
+++ client/src/routes/(menus)/lobby/[lid]/game/+page@.svelte
@@ -1,8 +1,16 @@
 <script>
-    import Button from '$lib/Button.svelte';
+    const players = ['a', 'b', 'c', 'd', 'e'];
 </script>
 
-<p>GAME SCREEN</p>
-<Button content="Zip" />
-<Button content="Zap" />
-<Button content="Zop" />
+<div class="flex h-1/2 w-2/3 flex-col items-center justify-between gap-4">
+    <div class="grid h-full w-full grid-flow-col">
+        {#each players as player}
+            <div class="text-center text-4xl hover:bg-slate-300">{player}</div>
+        {/each}
+    </div>
+    <div>
+        <button class="rounded-2xl border-2 p-4 text-xl">Zip</button>
+        <button class="rounded-2xl border-2 p-4 text-xl">Zap</button>
+        <button class="rounded-2xl border-2 p-4 text-xl">Zop</button>
+    </div>
+</div>
