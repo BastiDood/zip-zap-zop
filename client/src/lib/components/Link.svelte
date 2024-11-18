38196524c4c23d6f4dddbccd95c7cde8b9fe74eb
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-11-19 01:03:34 +0800

    refactor(client): require children in `Link` props

--- client/src/lib/components/Link.svelte
+++ client/src/lib/components/Link.svelte
@@ -3,10 +3,10 @@
     import type { Snippet } from 'svelte';
 
     interface Props extends Omit<HTMLAnchorAttributes, 'class'> {
-        children?: Snippet;
+        children: Snippet;
     }
 
     const { children, ...rest }: Props = $props();
 </script>
 
-<a {...rest} class="rounded-lg border-2 border-solid px-8 py-2 text-xl">{@render children?.()}</a>
+<a {...rest} class="rounded-lg border-2 border-solid px-8 py-2 text-xl">{@render children()}</a>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -5,8 +5,8 @@
 <div class="flex flex-col items-center gap-2">
     <h1 class="p-8 text-6xl">Zip Zap Zop!</h1>
     <div class="grid grid-flow-col gap-4">
-        <Link href="/create">Create Lobby</Link>
-        <Link href="/join">Join Lobby</Link>
-        <Link href="/help">Help</Link>
+        <Link href="/create/">Create Lobby</Link>
+        <Link href="/join/">Join Lobby</Link>
+        <Link href="/help/">Help</Link>
     </div>
 </div>
