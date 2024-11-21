c1351ab4a11c6b0c461eb8f601391f618852f2dc
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-11-22 02:53:25 +0800

    feat(ui): center title screen

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<div class="flex flex-col items-center gap-2">
-    <h1 class="p-8 text-6xl">Zip Zap Zop!</h1>
+<div class="flex h-screen flex-col items-center justify-center gap-2">
+    <h1 class="p-8 text-5xl md:text-6xl">Zip Zap Zop!</h1>
     <div class="grid grid-flow-col gap-4">
         <a class="btn btn-primary" href="/create/">Create Lobby</a>
         <a class="btn btn-secondary" href="/join/">Join Lobby</a>
