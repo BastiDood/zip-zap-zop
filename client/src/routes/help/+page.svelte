f962eed26a1a4e72451a2cedf8dca05ca0263964
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-12-04 23:10:51 +0800

    fix(ui): use typography plugin properly

--- client/src/routes/help/+page.svelte
+++ client/src/routes/help/+page.svelte
@@ -1,12 +1,12 @@
-<div class="flex h-screen flex-col items-center justify-center gap-4 px-12">
-    <div class="lg:prose-xl prose space-y-4 text-left">
-        <h2 class="text-3xl">Help</h2>
+<div class="flex flex-col items-center justify-center p-12">
+    <div class="prose max-w-full">
+        <h2>Help</h2>
         <p>
             Zip Zap Zop is a very simple game. During each turn, a player has three ways to respond: <strong>Zip</strong
             >,
             <strong>Zap</strong>, or <strong>Zop</strong> (in that order specifically!).
         </p>
-        <ul class="list-inside list-disc">
+        <ul>
             <li>Player A says <strong>Zip</strong> and points to Player C.</li>
             <li>Player C says <strong>Zap</strong> and points to Player B.</li>
             <li>Player B says <strong>Zop</strong> and points to Player C.</li>
@@ -14,7 +14,7 @@
             <li>And the cycle goes so on...</li>
         </ul>
         <p>A player is eliminated if:</p>
-        <ol class="list-inside list-decimal">
+        <ol>
             <li>They took too long to respond, or</li>
             <li>They responded in violation of the strict Zip-Zap-Zop order.</li>
         </ol>
@@ -23,6 +23,6 @@
             on).
         </p>
         <p>The last player standing wins the game!</p>
-        <a class="btn btn-primary" href="/">Okay!</a>
+        <a class="btn btn-primary text-primary-content" href="/">Okay!</a>
     </div>
 </div>
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,9 +1,10 @@
 import type { Config } from 'tailwindcss';
 import DaisyUi from 'daisyui';
+import typo from '@tailwindcss/typography';
 
 export default {
     experimental: { optimizeUniversalDefaults: true },
     content: ['./src/**/*.{html,js,svelte,ts}'],
-    plugins: [DaisyUi],
-    daisyui: { themes: ['dim'] },
+    plugins: [DaisyUi, typo],
+    daisyui: { themes: ['dim'], logs: false },
 } satisfies Config;
