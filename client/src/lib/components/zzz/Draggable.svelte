f63174e4825675aadf4a298a916665891c32358d
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2025-02-20 07:27:28 +0800

    feat(ui): add drag and drop components

---
+++ client/src/lib/components/zzz/Draggable.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { CSS } from '@dnd-kit-svelte/utilities';
+    import { useDraggable } from '@dnd-kit-svelte/core';
+
+    const { id } = $props();
+    const { transform, listeners, attributes, node } = useDraggable({ id });
+    const style = $derived(transform.current ? `transform: ${CSS.Translate.toString(transform.current)}` : '');
+
+    function buttonColorClasses(id: string) {
+        switch (id) {
+            case 'Zip':
+                return 'btn-info';
+            case 'Zap':
+                return 'btn-success';
+            case 'Zop':
+                return 'btn-warning';
+        }
+    }
+</script>
+
+<div
+    class="btn btn-circle btn-lg z-10 ring-offset-neutral {buttonColorClasses(id)}"
+    {style}
+    bind:this={node.current}
+    {...attributes.current}
+    {...listeners.current}
+>
+    {id}
+</div>
---
+++ client/src/lib/components/zzz/Droppable.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { type UseDroppableArguments, useDroppable } from '@dnd-kit-svelte/core';
+    import type { Snippet } from 'svelte';
+
+    interface DroppableProps extends UseDroppableArguments {
+        children: Snippet;
+    }
+
+    const { children, ...rest }: DroppableProps = $props();
+    const droppable = useDroppable(rest);
+</script>
+
+<div
+    class="rounded-xl border bg-base-300 px-4 py-2 text-neutral-content {droppable.isOver.current
+        ? 'animate-pulse border-neutral-content'
+        : 'border-base-300'}"
+    bind:this={droppable.node.current}
+>
+    {@render children()}
+</div>
