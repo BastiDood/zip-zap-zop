2977f5705a69ddfe47aa4d671726bed0560009dd
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2025-02-21 21:12:34 +0800

    fix(client): add polyfill for `Symbol.dispose`
    See msgpack/msgpack-javascript#260.

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script>
     import './app.css';
+    import { onMount } from 'svelte';
+
     const { children } = $props();
+
+    onMount(() => {
+        // https://github.com/msgpack/msgpack-javascript/issues/260
+        if (Object.hasOwn(Symbol, 'dispose')) return;
+        Object.defineProperty(Symbol, 'dispose', {
+            value: Symbol('zzz:dispose'),
+            configurable: false,
+            writable: false,
+            enumerable: false,
+        });
+    });
 </script>
 
 {@render children()}
