10ae297472070452a9a8d76d2c9840722d1713ec
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2025-02-27 21:15:02 +0800

    refactor(ui): import GitHub icon

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+    import GitHub from '@iconify/icons-simple-icons/github';
     import Icon from '@iconify/svelte';
 </script>
 
 <header class="absolute right-0 top-0 p-4">
     <a href="https://github.com/BastiDood/zip-zap-zop">
-        <Icon icon="mdi:github" width="48" />
+        <Icon icon={GitHub} width="40" />
     </a>
 </header>
 <div class="flex h-screen flex-col items-center justify-center">
