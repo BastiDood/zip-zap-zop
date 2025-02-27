70c59907cb1f4091b402cfd969ba2ef29fbe9f9d
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2025-02-27 19:29:59 +0800

    feat(ui): add link to GitHub repo

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,4 +1,13 @@
-<div class="flex h-screen flex-col items-center justify-center gap-2">
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+</script>
+
+<header class="absolute right-0 top-0 p-4">
+    <a href="https://github.com/BastiDood/zip-zap-zop">
+        <Icon icon="mdi:github" width="48" />
+    </a>
+</header>
+<div class="flex h-screen flex-col items-center justify-center">
     <h1 class="p-8 text-5xl md:text-6xl">Zip Zap Zop!</h1>
     <div class="grid grid-flow-col gap-4">
         <a class="btn btn-primary" href="/create/">Create Lobby</a>
